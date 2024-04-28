186bfdcfb363b8cdc9e36f65795491b821314180
Bagi763 <166919061+Bagi763@users.noreply.github.com>
2024-04-28 17:29:35 -0400

    added the real file

---
+++ app.py
@@ -0,0 +1,43 @@
+#Inicialize
+todo = []
+print("Welcome To Py-Todo APP!!")
+
+#Define Functions
+def add(Task):
+    todo.append(Task)
+    print(f"\nThe Task '{Task}' was Added to The ToDo List!")
+
+def edit(Task, Edit):
+    todo.insert(todo.index(Task), Edit)
+    todo.remove(Task)
+    print(f"The Task: '{Task}' is Now '{Edit}'!!")
+
+def delete(Task):
+    todo.remove(Task)
+    print(f"The Task: {Task} has been Deleted!!")
+
+#Run Program
+while True:
+    print("\nWhat Command You Want to Run?")
+    print("-------------------------------")
+    print("List: Show The ToDo List")
+    print("Add: Add a Task to The ToDo List")
+    print("Edit: Edit a Task on The ToDo List")
+    print("Delete: Delete a Task on The ToDo List")
+    print("Quit: Quit The APP")
+    command = input("Command: ")
+    if command == "List" or command == "list":
+        print(f"\n{todo}")
+    elif command == "Add" or command == "add":
+        add(input("Task: "))
+    elif command == "Edit" or command == "edit":
+        print(f"\nWhat Task You Want to Edit (Insert Task Number on The List)?\n{todo}")
+        edit(input("Task: "), input("Edit: "))
+    elif command == "Delete" or command == "delete":
+        print(f"\nWhat Task You Want to Delete?\n{todo}")
+        delete(input("Task: "))
+    elif command == "Quit" or command == "quit":
+        break
+    else:
+        print("\nInvalid Command, Try Again")
+print("Bye!!")
